a367446deea8da9eedc6a78a7e0212702bad253d
SYY99020 <46041249+SYY99020@users.noreply.github.com>
2020-11-03 22:46:30 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,88 @@
+package com.atguigu.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author SYY
+ * @date 2020/11/2
+ */
+public class Customer {
+
+    // psvm
+    public static void main(String[] args) {
+        // sout
+        System.out.println("MODULE!");
+        // soutm
+        System.out.println("Customer.main");
+        int n = 10;
+        // soutv
+        System.out.println("n = " + n);
+        // n.sout
+        System.out.println(n);
+        // fori
+        for (int i = 0; i < 10; i++) {
+
+        }
+        // iter
+        for (String arg : args) {
+
+        }
+        // itar
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+
+        }
+        List list = new ArrayList<>();
+        // list.for
+        for (Object o : list) {
+
+        }
+        // list.fori
+        for (int i = 0; i < list.size(); i++) {
+
+        }
+        // list.forr
+        for (int i = list.size() - 1; i >= 0; i--) {
+
+        }
+        // 10.fori
+        for (int i = 0; i < 10; i++) {
+
+        }
+        // 10.for 10.forr
+        for (int i = 10; i > 0; i--) {
+
+        }
+        // ifn
+        if (list == null) {
+
+        }
+        // inn
+        if (list != null) {
+
+        }
+        // list.nn
+        if (list != null) {
+
+        }
+        // list.null
+        if (list == null) {
+
+        }
+    }
+
+    // prsf
+    private static final int a = 0;
+
+    // psf
+    public static final int b = 1;
+
+    // psfi
+    public static final int c = 2;
+
+    // psfs
+    public static final String d = "3";
+
+
+}
---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.atguigu.java;
+
+/**
+ * @author SYY
+ * @date 2020/11/2
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("helloworld!!");
+    }
+
+}
